0858fccb63480f35e9e2c346858687bb53d28926
JorgeTheCreator <jrgpeguero@gmail.com>
2019-11-04 13:25:18 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.listen((process.env.PORT || 5000));
+
+// Server index page
+app.get("/", function (req, res) {
+  res.send("Deployed!");
+});
+
+// Facebook Webhook
+// Used for verification
+app.get("/webhook", function (req, res) {
+  if (req.query["hub.verify_token"] === "this_is_my_token") {
+    console.log("Verified webhook");
+    res.status(200).send(req.query["hub.challenge"]);
+  } else {
+    console.error("Verification failed. The tokens do not match.");
+    res.sendStatus(403);
+  }
+});
